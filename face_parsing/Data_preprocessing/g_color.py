65d041c5786a591f5b924e7ae6d9ffd074dd549b
Raghurama G <56394666+rv-gonela@users.noreply.github.com>
2020-10-12 01:31:09 +0530

    corrected typo in line 8

--- face_parsing/Data_preprocessing/g_color.py
+++ face_parsing/Data_preprocessing/g_color.py
@@ -5,7 +5,7 @@ import numpy as np
 from utils import make_folder
 color_list = [[0, 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51, 0], [255, 153, 51], [0, 204, 0]]
 
-folder_base = 'CelebAMask-HQ-mask'
+folder_base = 'CelebAMaskHQ-mask'
 folder_save = 'CelebAMask-HQ-mask-color'
 img_num = 10
 
