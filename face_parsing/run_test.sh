a868c5af014d19ea3aa11042c2369284561c4fd0
Raghurama G <rv.gonela@gmail.com>
2020-11-02 00:26:31 +0530

    removed the prefixes in the shell files

--- face_parsing/run_test.sh
+++ face_parsing/run_test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1 python -u main.py --batch_size 2 --imsize 512 --version parsenet --train False
+python -u main.py --batch_size 2 --imsize 512 --version parsenet --train False
